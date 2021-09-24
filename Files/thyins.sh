5c543bfe4252a69955c846033ba357ad11c3639c
Devil Master <72663288+isuruwa@users.noreply.github.com>
2021-09-24 10:32:05 +0530

    Update thyins.sh

--- Files/thyins.sh
+++ Files/thyins.sh
@@ -5,5 +5,5 @@ make
 make install
 chmod +x hydra
 chmod +x thydra
-cp -r hydra /data/data/com.termux/files/usr/bin
-cp -r thydra /data/data/com.termux/files/usr/bin
+cp -r hydra $PREFIX/bin
+cp -r thydra $PREFIX/bin
